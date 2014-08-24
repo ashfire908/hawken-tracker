fd71410823be17e45bf1094905c7b4cd76b698fb
Andrew Hampe <andrew.hampe@gmail.com>
2014-08-23 23:37:26 -0500

    Properly parse the port argument for tracker-web

--- tracker-web.py
+++ tracker-web.py
@@ -10,7 +10,7 @@ import hawkentracker.views
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Runs a server for the tracker web interface")
     parser.add_argument("--host", help="sets the host to listen for")
-    parser.add_argument("--port", help="sets the port to listen for")
+    parser.add_argument("--port", type=int, help="sets the port to listen for")
 
     args = parser.parse_args()
     app.run(host=args.host, port=args.port, debug=app.debug)
