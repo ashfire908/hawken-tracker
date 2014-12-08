fee3278350d2ae57b6d0857a822009c01e5fefba
Andrew Hampe <andrew.hampe@gmail.com>
2014-12-08 17:10:23 -0600

    Detect and apply fix for external debuggers

--- tracker-web.py
+++ tracker-web.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import inspect
+import logging
 import argparse
 from hawkentracker import create_app
 
@@ -11,6 +13,16 @@ if __name__ == "__main__":
     parser.add_argument("--debug", action="store_true", default=None)
 
     args = parser.parse_args()
+    opts = {}
 
     app = create_app()
-    app.run(host=args.host, port=args.port, debug=args.debug)
+
+    # I call hax.
+    for frame in inspect.stack():
+        if frame[1].endswith("pydevd.py"):
+            logging.info("External debugger detected. Disabling reloader.")
+            opts["use_reloader"] = False
+            break
+
+    app.run(host=args.host, port=args.port, debug=args.debug, **opts)
+
